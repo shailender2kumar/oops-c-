ceb96b70b771940654d394a5ca5d8aeeed7086ae
shailender <shailendersaini2003@gmail.com>
2022-02-16 12:27:53 +0530

    9febc++.cpp

---
+++ 9febc++.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+/*int main()
+{
+   std:: cout<<"hello world";
+    return 0;
+}*/
+using namespace std;
+ void ALLOPERATION(int e, int f){
+       
+       cout<<"\nadd:"<<e+f;
+        cout<<"\ndivision:"<<e/f;
+         cout<<"\nminus:"<<e-f;
+          cout<<"\nmultiply:"<<e*f;
+
+   }
+int main()
+{
+    
+   /* int a, b, c;
+    cout<<"enter the number to be add";
+    cin>>a>>b;
+    c=a+b;
+    cout<<"sum is"<<c;*/
+   ALLOPERATION(4,2);
+    //return 0;
+}
+

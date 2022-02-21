de1a478e62ab08db9523b38e794c44cfbb831967
shailender <shailendersaini2003@gmail.com>
2022-02-21 20:31:07 +0530

    class.cpp

---
+++ class.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class smallobj
+{
+    private:
+    int somedata ;
+    public:
+    void setdata(int D)
+    {
+        somedata = D;
+    }
+    void showdata()
+    {
+        cout<<"data is "<<somedata<<endl;
+    }
+
+};
+int main()
+{
+    smallobj s1,s2;
+    s1.setdata(1066);
+    s2.setdata(1776);
+    s1.showdata();
+    s2.showdata();
+    return 0;
+}
+
+
+

80a842f3d04c9982ea16c4f9e735b037f4b0ce89
shailender kumar 202120 <84677427+shailender2kumar@users.noreply.github.com>
2022-05-17 21:30:36 +0530

    Update Cpp16.cpp

--- Cpp16.cpp
+++ Cpp16.cpp
@@ -1,43 +1,23 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
- 
-class Distance {
-   private:
-      int feet;             
-      int inches;           
-      
-   public:
-     //constructors
-      Distance() {
-         feet = 0;
-         inches = 0;
-      }
-      Distance(int f, int i) {
-         feet = f;
-         inches = i;
-      }
-      
-      // display distance
-      void displayDistance() {
-         cout << "F: " << feet << " I:" << inches <<endl;
-      }
-      
-      // overloaded minus (-) operator
-      Distance operator- () {
-         feet = -feet;
-         inches = -inches;
-         return Distance(feet, inches);
-      }
+class Count{
+    private:
+    float value;
+    public:
+    Count(){
+        value =5;
+    }
+    void operator -- () {
+    value =value/2;
+        
+    }
+    void display(){
+        cout<<"ans "<<value<<endl;
+    }
 };
-
-int main() {
-   Distance D1(11, 10), D2(-5, 11);
- 
-   -D1;                     // apply 
-   D1.displayDistance();    // display D1
-
-   -D2;                     // apply 
-   D2.displayDistance();    // display D2
-
+int main(){
+   Count count1;
+   --count1;
+   count1.display();
    return 0;
 }

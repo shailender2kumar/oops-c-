0948fafbea47ba874a957b6d2318647e1e1fa260
shailender kumar 202120 <84677427+shailender2kumar@users.noreply.github.com>
2022-05-29 23:18:14 +0530

    Create cpp21.cpp

---
+++ cpp21.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	fstream file;
+	ifstream ifile("file.txt", ios::in);
+	ofstream ofile("file2.txt", ios::out | ios::app);
+	if (!ifile.is_open()) {
+		cout << "file not found";
+	}
+	else {
+		ofile << ifile.rdbuf();
+	}
+	string word;
+	file.open("file2.txt");
+	while (file >> word) {
+		cout << word << " ";
+	}
+	return 0;
+}
